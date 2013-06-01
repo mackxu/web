5208128194e746ce3494e2a3037c20331cd5832e
mackxu <1004929678@qq.com>
2013-06-01 11:33:49 +0800

    键盘事件 处理上箭头、下箭头、Esc、回车键

--- search2/search.js
+++ search2/search.js
@@ -10,6 +10,7 @@
 		this.timeoutId = null;					// 计时器id
 		this.cur = -1;							// 当前选择的建议项，用于填充字段
 		this.layer = null;						// 建议列表的外<div/>, 主要用于隐藏与显示
+		this.searchText = '';					// 发送ajax请求时，用户输入的文本
 
 		this.init();
 	}
@@ -41,7 +42,7 @@
 		this.layer = $('<div></div>')
 			.addClass('suggestions')
 			.hide()
-			.css('width', this.textbox.width())
+			.css('width', this.textbox.outerWidth())
 			.appendTo('body');
 		
 		var oSelf = this;
@@ -84,28 +85,77 @@
 	// 处理字符键向服务器请求数据
 	AutoSuggestControl.prototype.handleKeyup = function(oEvent) {
 		var iKeyCode = oEvent.keyCode;
+		this.searchText = this.textbox.val();				// 向服务器发送的文本
 		oSelf = this;
 
 		clearTimeout(this.timeoutId);
-		this.timeoutId = setTimeout(function() {
-			oSelf.provider.requestSuggestions(oSelf, true);
-		}, 250);
+
+		if (iKeyCode ==8 || iKeyCode == 46) {				// 处理删除键和退格键
+			this.timeoutId = setTimeout(function() {
+				oSelf.provider.requestSuggestions(oSelf, false);
+			}, 250);
+		}else if((iKeyCode!=16 && iKeyCode<32) || (iKeyCode>32 && iKeyCode<46)
+				|| (iKeyCode>=112 && iKeyCode<=123)) {
+			// 32 空格键
+			// do nothing
+		}else {												// 排除了非字符键后
+			this.timeoutId = setTimeout(function() {
+				oSelf.provider.requestSuggestions(oSelf, true);
+			}, 250);
+		}		
+	}
+	// 用于处理上箭头、下箭头、Esc、回车键
+	// 处理键盘事件一种技巧
+	AutoSuggestControl.prototype.handleKeydown = function(oEvent) {
+		switch(oEvent.keyCode) {
+			case 38: 	// 上箭头
+				this.goToSuggestion(-1);					// 选择前一个建议
+				break;
+			case 40: 	// 下箭头
+				this.goToSuggestion(1);						// 选中下一个建议
+				break;
+			case 27: 	// Esc
+				this.textbox.val(this.searchText);			// 清除补充的文本
+				// 将光标放在用户输入文本的后面，等待继续输入
+
+			case 13: 	// 回车键
+				this.hideSuggestions();						// 隐藏下拉建议
+				oEvent.preventDefault();					// 取消默认行为
+		}
 	}
 
-	AutoSuggestControl.prototype.handleKeydown = function(oEvent) {
-		
+	AutoSuggestControl.prototype.goToSuggestion = function(iDiff) {
+		var $suggestions = this.layer.children('div');		// 所有建议项集合
+
+		if ($suggestions.length > 0) {
+			var $suggestion = null;
+			if (iDiff > 0) {								// 处理下箭头事件
+				if (this.cur < $suggestions.length-1) {		// 确保最大边界
+					$suggestion = $suggestions.eq(++this.cur);
+				}
+			}else {											// 上箭头事件
+				if (this.cur > 0) {							// 确保最小边界
+					$suggestion = $suggestions.eq(--this.cur);
+				}
+			}
+			if ($suggestion) {								// 当用键盘，选中了建议项
+				this.highlightSuggestion($suggestion);		// 高亮当前建议项
+				this.textbox.val($suggestion.text());		// 填充字段
+			}
+		}
+
 	}
 
 	// 接收服务器端发来的数据
 	// 并决定在页面中显示
 	AutoSuggestControl.prototype.autosuggest = function(aSuggests, bTypeAHead) {
-		this.cur = -1;				// 每次显示建议前，重置当前指针
+		this.cur = -1;										// 每次显示建议前，重置当前指针
 
-		if(aSuggests.length > 0) {
+		if(aSuggests.length > 0) {							// 存在建议项
 
-			this.showSuggestions(aSuggests);			// 存在建议项，显示
-		}else {											// 否则，
-			this.hideSuggestions();						// 隐藏建议列表
+			this.showSuggestions(aSuggests);				// 显示
+		}else {												// 否则，
+			this.hideSuggestions();							// 隐藏建议列表
 		}
 	}
 
@@ -125,7 +175,7 @@
 		});
 
 		var oPosition = this.textbox.offset();				// 搜索框坐标
-		var iHeight = this.textbox.height();
+		var iHeight = this.textbox.outerHeight();
 		// 定位建议列表
 		this.layer.css({'left': oPosition.left, 'top': oPosition.top + iHeight});
 		// 显示建议列表
@@ -133,10 +183,12 @@
 	}
 	// 高亮建议项
 	AutoSuggestControl.prototype.highlightSuggestion = function($suggestion) {
+
 		// 去除之前的高亮项，高亮当前项
 		$suggestion.addClass('current').siblings('div').removeClass('current');
 	}
 
+	//------------------------------ class SuggestionProvider ------------------
 	var SuggestionProvider = {};
 	SuggestionProvider.oXHR = null;
 
@@ -144,19 +196,18 @@
 	// 静态方法
 	SuggestionProvider.requestSuggestions = function(oAutoSuggestControl, bTypeAHead) {
 		var oXHR = SuggestionProvider.oXHR;
-		// 取消所有进行中的请求
+		// 当建立新的请求时，取消所有进行中的请求
 		if (oXHR && oXHR.readyState != 0) {
 			oXHR.abort();
 		}
 		SuggestionProvider.oXHR 
-			= $.getJSON('suggestion.php', {})
+			= $.getJSON('suggestion.php', {'s': oAutoSuggestControl.searchText})
 			.done(function(data, stateText, jqXHR) {
-				// console.log(data, stateText);
 				// 为控件提供建议
 				oAutoSuggestControl.autosuggest(data, bTypeAHead);
 
 			}).fail(function(jqXHR, stateText, error) {
-				
-				console.log(stateText, error);
+				// 请求错误时提示
+				console.error(stateText+ ': '+error);
 			});
 	}
--- search2/suggestion.php
+++ search2/suggestion.php
@@ -1,4 +1,29 @@
 <?php
 	// 建议获取
-	$suggestions  = array('a', 'b', 'c');
-	echo json_encode($suggestions);
+	$suggestions  = array(
+		'张三丰',
+		'张无忌',
+		'张三',
+		'郑微',
+		'林静',
+		'阮莞',
+		'陈孝正',
+		'张开',
+		'许开阳'
+	);
+	// 相关联的关键字
+	$possible_keys = array();
+	// 搜索关键字
+	if (isset($_GET['s'])) {
+		$search = strtolower($_GET['s']);
+		if (!empty($search)) {
+			foreach ($suggestions as $suggestion) {
+				// 匹配以搜索文本开头的关键字
+				if (strpos($suggestion, $search) === 0) {
+					$possible_keys[] = $suggestion;
+				}
+			}
+		}
+	}
+	// 以json格式传递数据
+	echo json_encode($possible_keys);
