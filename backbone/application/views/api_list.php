68b6391f9da6eae010c8f3cdda993dbaef04297a
mackxu <1004929678@qq.com>
2013-12-04 20:18:27 +0800

    图片展示，需要弄懂它们的执行顺序

--- backbone/application/views/api_list.php
+++ backbone/application/views/api_list.php
@@ -2,23 +2,10 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
-
-	<style type="text/css">
-
-	::selection{ background-color: #E13300; color: white; }
-	::moz-selection{ background-color: #E13300; color: white; }
-	::webkit-selection{ background-color: #E13300; color: white; }
-
-	body { background-color: #fff; margin: 40px; font: 13px/20px normal Helvetica, Arial, sans-serif; color: #4F5155; }
-
-	h1 {color: #444;background-color: transparent;border-bottom: 1px solid #D0D0D0;ont-size: 19px;ont-weight: normal;margin: 0 0 14px 0;padding: 14px 15px 10px 15px; }
-	#body{margin: 0 15px 0 15px;}
-	
-	p.footer{text-align: right;font-size: 11px;border-top: 1px solid #D0D0D0;line-height: 32px;padding: 0 10px 0 10px;margin: 20px 0 0 0;}
-	
-	#container{ margin: 10px;border: 1px solid #D0D0D0;-webkit-box-shadow: 0 0 8px #D0D0D0; }
-	</style>
+	<title>Welcome to Backbone</title>
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<link rel="stylesheet" type="text/css" href="static/css/base.css">
+	<link rel="stylesheet" type="text/css" href="static/css/api.css">
 	<script type="text/javascript" src="static/js/libs/underscore.js"></script>
 	<script type="text/javascript" src="static/js/libs/zepto.js"></script>
 	<script type="text/javascript" src="static/js/libs/backbone.js"></script>
@@ -34,9 +21,16 @@
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
 </div>
+<script type="text/template" id="imageItem">
+	<div><a href="#view/<%= id %>"><img src="static/<%= src %>"></a></div>
+	<p><%= intro %></p>
+</script>
 <script src="static/js/app.js"></script>
 <script>
-	$('#body').append(new App().render().el);
+	window.$body = $('#body');
+	// 开启Backbone之旅
+	new App();
+	Backbone.history.start();
 </script>
 
 </body>
--- backbone/static/js/app.js
+++ backbone/static/js/app.js
@@ -1,7 +1,73 @@
-var App = Backbone.View.extend({
-	tagName: 'h3',
+// 可以添加一个标识来判断显示大图还是小图
+var ImageMdl = Backbone.Model.extend({
+	defaults: {
+		'id': 0,
+		'src': '',
+		'src_origin': '',
+		'intro': ''
+	},
+	urlRoot: '/api/view',
+	initialize: function() {}
+});
+
+var ImagesC = Backbone.Collection.extend({
+	url: '/api/images',
+	model: ImageMdl,
+	initialize: function() {
+		this.fetch({'reset': true});
+	}
+});
+
+//============================ 视图 ===========
+var ImageItem = Backbone.View.extend({				// 列表中单个的视图, 对应一个模型
+	tagName: 'li',
+	template: _.template($('#imageItem').html()),
+	initialize: function() {
+		this.model.on('change', this.render, this);
+	},
 	render: function() {
-		this.$el.html('good work');
+		this.$el.html(this.template(this.model.toJSON()));		// 用数据渲染模板
 		return this;
+	}
+});
+
+var ImagesView = Backbone.View.extend({				// 图片列表视图， 对应一个集合
+	tagName: 'ul',
+	className: 'imageList',
+	initialize: function() {
+		this.collection.on('reset', this.render, this);
+	},
+	addItem: function(imageMdl) {
+		var imageItem = new ImageItem({ model: imageMdl });
+		this.$el.append(imageItem.render().el);		// 向列表添加条目
+	},
+	render: function() {
+		this.collection.each(this.addItem, this);
+		return this;
+	}
+});
+
+// 大图展示页面
+// 采用不同的模板
+// var imageBigItem = Backbone.View.extend({  });
+
+// 创建路由
+var App = Backbone.Router.extend({
+	routes: {
+		'': 'list',
+		'view/:id': 'item'
+	},
+	list: function() {
+		var images = new ImagesC();					// 创建集合并请求数据
+		var imagesView = new ImagesView({ collection: images });
+		$body.html(imagesView.render().el);	// 渲染页面
+	},
+	item: function(id) {
+		var imageMdl = new ImageMdl({id: id});
+		var imageItem = new ImageItem({
+			el: $body,
+			model: imageMdl
+		});
+		imageMdl.fetch();
 	}
 });
