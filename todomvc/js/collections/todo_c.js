593642a451b73bf0589666f617ede20b820bc682
mackxu <1004929678@qq.com>
2013-12-07 11:31:16 +0800

    添加事件需要在触发之前添加

--- todomvc/js/collections/todo_c.js
+++ todomvc/js/collections/todo_c.js
@@ -1,7 +1,21 @@
 var Todos = Backbone.Collection.extend({
-	model: TodoMdl,
-	localStorage: new Backbone.LocalStorage('todo'),
-	comparator: 'order',
+	// model: TodoMdl,
+	// localStorage: new Backbone.LocalStorage('todo'),
+	comparator: 'title',
 	done: function() {},
 	remaining: function() {}
 });
+
+var todos = new Backbone.Collection([{ id: 1, title: 'qqqqqq', completed: false }, { id: 2, title: 'wwwwww', completed: true }]);
+
+// todos.add({ id: 1, title: 'qqqqqq', completed: false });
+// todos.add({ id: 2, title: 'wwwwww', completed: true });
+
+// todos.add([{ id: 1, title: 'qqqqqq', completed: false }, { id: 2, title: 'wwwwww', completed: true }]);
+
+todos.on('add', function(model) {
+	console.log(model);
+});
+todos.add({ id: 3, title: 'eeeeeee', completed: false });
+
+console.log(todos.toJSON());
