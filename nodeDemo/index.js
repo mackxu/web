716066cc71bcbe896fdf1da877203e2335a67ab4
mackxu <xydjob@gmail.com>
2015-07-23 13:13:48 +0800

    一些注释

--- nodeDemo/index.js
+++
@@ -1,4 +0,0 @@
-var server = require('./server');
-var router = require('./router');
-// 开启HTTP服务器
-server.start(router.route);
--- nodeDemo/router.js
+++
@@ -1,5 +0,0 @@
-function route(pathname) {
-	console.log(pathname);
-}
-
-exports.route = route;
