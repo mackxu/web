85c580977201d9264cd40592d964d9e5289bb0b1
xuyadong <xydjob@gmail.com>
2016-03-03 14:32:49 +0800

    javascript设计模式与开发实战的练习

--- marionette/members.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	$members = array(
-		array('id'=> 1, 'firstName'=> 'aaa', 'lastName'=> '111'),
-		array('id'=> 2, 'firstName'=> 'bbb', 'lastName'=> '222'),
-		array('id'=> 5, 'firstName'=> 'eee', 'lastName'=> '555'),
-	);
-
-	echo json_encode($members);
