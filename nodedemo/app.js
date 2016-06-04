c318626bd52557aca5118461dae58ed1ae24c060
mackxu <xydjob@gmail.com>
2016-06-04 22:59:38 +0800

    use package

--- nodedemo/app.js
+++ nodedemo/app.js
@@ -1,8 +1,17 @@
 var express = require('express');
+var utility = require('utility');
+
 var app = express();
 
 app.get('/', function(req, res) {
-	res.send('Hello World');
+	// res.send('Hello World');
+	var q = req.query.q;
+
+	console.log(q);
+
+	var md5Value = utility.md5(q || '');
+
+	res.send(md5Value);
 });
 
 app.listen(3000, function() {
