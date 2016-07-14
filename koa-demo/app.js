558647019103ba6f393a1cb764b7f2d709d5f26d
xuyadong <xydjob@gmail.com>
2016-07-14 17:37:26 +0800

    demo for koa crud

--- koa-demo/app.js
+++ koa-demo/app.js
@@ -1,24 +1,93 @@
+
+var logger = require('koa-logger');
+var views = require('co-views');
+var parse = require('co-body');
+var router = require('koa-router')();
 var koa = require('koa');
-var router = require('koa-router');
-var Promise = require('bluebird');
-var fs = Promise.promisifyAll(require('fs'));           // 创建promise版本的fs
 
 var app = koa();
 
+// data store
+var todos = [];
 
-app.use(function *(next) {
-    console.log('start time => ', +new Date());
-    yield next;
-    console.log('end time => ', +new Date());
-});
+app.use(logger());
+
+// router middleware
+
+router.get('/', list);
+router.get('/todo/new', add);
+router.get('/todo/:id', show);
+router.get('/todo/del/:id', remove);
+router.get('/todo/edit/:id', edit);
+
+router.post('/todo/create', create);
+router.post('/todo/update', update);
+
+app.use(router.routes()).use(router.allowedMethods());
+
+// Specifying Swig view engine
+var render = views(__dirname + '/views', { map: { html: 'swig' } });
+
+/**
+ * Todo item List.
+ */
+function *list() {
+    this.body = yield render('index', { todos: todos });
+}
+
+/**
+ * Form for create new todo item.
+ */
+function *add() {
+    this.body = yield render('new');
+}
+
+/**
+ * Show details of a todo item.
+ */
+function *show() {
+
+}
 
 
-app.use(function *(){
-    // this.body = 'Hello World';
+/**
+ * Remove a todo item
+ */
 
-    // var path = this.path;
-    this.body = yield fs.readFileAsync('./app.js', 'utf8');
-});
+function *remove() {
+
+}
+
+/**
+ * Form for edit a todo items.
+ */
+function *edit() {
+
+}
+
+/**
+ * Create a todo item into the data store.
+ */
+function *create() {
+    var todo = yield parse(this);
+
+    todo.createdOn = +new Date();
+    todo.updatedOn = +new Date();
+
+    var id = todos.push(todo);
+    todo.id = id - 1;
+
+    this.redirect('/');
+}
+
+/**
+ * Update an existing todo item.
+ */
+function *update() {
+
+}
+
+
 
 app.listen(3000);
-console.log('now listening on port 3000');
+console.log('listening on port 3000');
