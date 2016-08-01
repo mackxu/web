ab9c43402e5914b3d41726635fdcb2e82d3a1bcb
xuyadong <xydjob@gmail.com>
2016-08-01 10:02:45 +0800

    koa tv config

--- koa-demo/config.js
+++ koa-demo/config.js
@@ -1,1 +1,71 @@
-{"bg":[{"name":"vipvipvip"},{"name":"world"}]}
+callback({
+    "backgrounds": [
+        {
+            "id": 0,
+            "name": "bgDefault",
+            "desc": "默认",
+            "url": "http://static.ptqy.gitv.tv/tv/vip/images/bg/buy_bg.jpg",
+            "updateTime": 0
+        },{
+            "id": 1,
+            "name": "bgVipDefault",
+            "desc": "VIP套餐-默认运营图",
+            "url": "http://static.ptqy.gitv.tv/tv/vip/images/bg/vip_default.jpg",
+            "updateTime": 0
+        },{
+            "id": 2,
+            "name": "bgVipGoldSale",
+            "desc": "VIP套餐-黄金会员首月折扣",
+            "url": "http://static.ptqy.gitv.tv/tv/vip/images/bg/vip_gold_sale.jpg",
+            "updateTime": 0
+        },{
+            "id": 3,
+            "name": "bgVipGold",
+            "desc": "VIP套餐-黄金会员",
+            "url": "http://static.ptqy.gitv.tv/tv/vip/images/bg/vip_gold.jpg",
+            "updateTime": 0
+        },{
+            "id": 4,
+            "name": "bgCouponDefault",
+            "desc": "点播VIP套餐：默认图",
+            "url": "http://static.ptqy.gitv.tv/tv/vip/images/bg/coupon_default.jpg",
+            "updateTime": 0
+        },{
+            "id": 5,
+            "name": "bgCouponGoldSale",
+            "desc": "点播VIP套餐：黄金首月折扣",
+            "url": "http://static.ptqy.gitv.tv/tv/vip/images/bg/coupon_gold_sale.jpg",
+            "updateTime": 0
+        },{
+            "id": 6,
+            "name": "bgCouponGold",
+            "desc": "点播VIP套餐：黄金",
+            "url": "http://static.ptqy.gitv.tv/tv/vip/images/bg/coupon_gold.jpg",
+            "updateTime": 0
+        },{
+            "id": 7,
+            "name": "vipRight",
+            "desc": "tv-用户权益页",
+            "url": "http://static.ptqy.gitv.tv/tv/vip/images/bg/vip_right.jpg",
+            "updateTime": 0
+        },{
+            "id": 8,
+            "name": "vipRightGold",
+            "desc": "tv-黄金用户权益页",
+            "url": "http://static.ptqy.gitv.tv/tv/vip/images/bg/vip_right_gold.jpg",
+            "updateTime": 0
+        },{
+            "id": 9,
+            "name": "mRightDefault",
+            "desc": "手机端-个人中心-用户权益",
+            "url": "http://static.ptqy.gitv.tv/tv/user/images/vip-privilege_member.jpg",
+            "updateTime": 0
+        },{
+            "id": 10,
+            "name": "mRightGold",
+            "desc": "手机端-个人中心-黄金会员权益",
+            "url": "http://static.ptqy.gitv.tv/tv/user/images/vip-privilege_puregold.jpg",
+            "updateTime": 0
+        }
+    ]
+});
