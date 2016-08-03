c6cd88f7de95a8f1ff79227f39bae1856f8d60f3
xuyadong <xydjob@gmail.com>
2016-08-03 19:36:48 +0800

    add files for prev

---
+++ vuemai/src/views/film/index.vue
@@ -0,0 +1,18 @@
+<template>
+</template>
+
+<script>
+export default {
+
+  name: 'component_filmList',
+
+  data () {
+    return {
+
+    };
+  }
+};
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ vuemai/src/views/home/index.vue
@@ -0,0 +1,18 @@
+<template>
+</template>
+
+<script>
+export default {
+
+  name: 'component_home',
+
+  data () {
+    return {
+
+    };
+  }
+};
+</script>
+
+<style lang="css" scoped>
+</style>
