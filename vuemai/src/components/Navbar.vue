222d00d1885a9a312eddee577a342d468fcb58dc
xuyadong <xydjob@gmail.com>
2016-08-16 18:53:52 +0800

    add sidebar and navbar

--- vuemai/src/components/Navbar.vue
+++ vuemai/src/components/Navbar.vue
@@ -1,19 +1,56 @@
 <template>
-    <nav class="app-nav"></nav>
+    <nav class="app-nav">
+    	<a href="javascript:;" class="fl aside" @click="showLeftNav()">
+    		<div class="fl navbar-title-icon"><i class="iconfont icon-list"></i></div>
+    		<h1 class="fl text-overflow title">爱奇艺电影</h1>
+    	</a>
+    	<div class="fr bside">
+    		<a href="#" v-link="{path:'/city'}" class="fl nav-city">
+    			<span class="city-content">北京</span>
+    			<i class="iconfont icon-dropdown"></i>
+    		</a>
+    		<a href="#" v-link="{path:'/center'}" class="fl nav-user">
+    			<i class="iconfont icon-user"></i>
+    		</a>
+    	</div>
+    </nav>
 </template>
 
 <script>
+
+import { showLeftNav } from 'actions'
+
 export default {
 
   name: 'component_navbar',
-
-  data () {
-    return {
-
-    };
+  vuex: {
+  	actions: {
+  		showLeftNav
+  	}
   }
 };
 </script>
 
 <style lang="css" scoped>
+.app-nav {
+	position: fixed; left: 0; right: 0; top: 0; z-index: 500;
+	overflow: hidden;
+	height: 1rem; line-height: 1rem;
+
+	background-color: #282828;
+}
+.navbar-title-icon {
+	width: 0.96rem;
+	border-right: 1px solid #222;
+	box-shadow: 1px 0 1px #363636;
+	color: #999;
+	text-align: center;
+}
+
+.title { 
+	color: #efefef; font-size: 0.28rem; padding: 0 0.2rem;
+	width: 3.2rem; 
+}
+.bside a { color: #999 }
+.nav-user { width: 0.96rem; text-align: center; }
 </style>
--- vuemai/src/components/Sidebar.vue
+++ vuemai/src/components/Sidebar.vue
@@ -1,21 +1,58 @@
 <template>
-    <aside class="app-sidebar">
-        
+    <aside class="app-sidebar container" v-show="leftNavState" transition="fade" @click="showLeftNav()">
+        <nav class="list-bg" v-show="leftNavState" transition="expand">
+    		<ul class="menu-list">
+    			<li class="menu-item"><a href="#" v-link="{path:'/'}"><span>首页</span><i class="iconfont icon-arrow-right"></i></a></li>
+    			<li class="menu-item"><a href="#" v-link="{path:'/films/now_playing'}"><span>影片</span><i class="iconfont icon-arrow-right"></i></a></li>
+    			<li class="menu-item"><a href="#" v-link="{path:'/'}"><span>影院</span><i class="iconfont icon-arrow-right"></i></a></li>
+    			<li class="menu-item"><a href="#" v-link="{path:'/member'}">我的<i class="iconfont icon-arrow-right"></i></a></li>
+    			<li class="menu-item"><a href="#" v-link="{path:'/card'}"><span>卖座卡查询</span><i class="iconfont icon-arrow-right"></i></a></li>
+    		</ul>
+    	</nav>
     </aside>
 </template>
 
 <script>
+
+import { getLeftNavState } from 'getters'
+import { showLeftNav } from 'actions'
+
 export default {
 
   name: 'component_sideBar',
 
-  data () {
-    return {
-
-    };
+  vuex: {
+  	getters: {
+  		leftNavState: getLeftNavState
+  	},
+  	actions: { showLeftNav }
   }
 };
 </script>
 
 <style lang="css" scoped>
+.container {
+	position: fixed; left: 0; right: 0; bottom: 0; top: 1rem;
+	background-color: rgba(0, 0, 0, .5);
+	z-index: 1;
+}
+.list-bg {
+	width: 4.2rem; height: 100%;
+	background-color: #282828;
+	box-shadow: 0 1px 1px #363636 inset;
+	border-top: 1px solid #222;
+}
+
+.menu-item { line-height: 1rem; }
+.menu-item a { display: block; padding: 0 0.32rem; border-bottom: 1px solid #333; color: #9a9a9a; font-size: 0.28rem; }
+.icon-arrow-right { color: #666; font-size: 0.24rem; float: right; }
+
+.fade-transition {
+	transition: all .5s ease;
+}
+.fade-enter, .fade-leave { opacity: 0 }
+
+.expand-transition { transition: width .4s ease; }
+.expand-enter, .expand-leave { width: 0 }
+
 </style>
