e8850c4d447b7e6038731e053f22b3d61511b61f
xuyadong <xydjob@gmail.com>
2017-04-10 15:52:07 +0800

    实现无线滚动加载

--- waterflow/app/index.js
+++ waterflow/app/index.js
@@ -2,20 +2,85 @@ import _ from 'lodash';
 import $ from 'jquery'
 import server from './server.js';
 
+let ITEM_AMOUNT = 25;
+let LIMIT_HEIGHT = 100;
 let $cols = [];
+let $win = $(window);
+let $doc = $(document);
+let dataReady = true;
+
 function main() {
   createContainers();
-  
-  server().then(function() {
-    console.log('data ready')
-  })
+  fetchAndRenderItem();
+
+  $win.on('scroll', _.throttle(onScroll, 100))
+}
+
+// 加载服务器数据，并绘制
+function fetchAndRenderItem() {
+	console.log('fetch data...')
+	dataReady = false;
+	server().then(function() {
+		dataReady = true;
+    	_.times(ITEM_AMOUNT, appendItem)
+  	})
+}
+
+function onScroll() {
+	// 判断页面是否滑到底部
+	if($doc.height() - $win.height() - $doc.scrollTop() > LIMIT_HEIGHT || !dataReady) {
+		return false;
+	}
+	fetchAndRenderItem();
 }
 
 // 动态创建容器
 function createContainers() {
   for (let i = 0, l = 4; i < l; i++) {
-    $cols[i] = $('<div class="container">1</div>')
+    $cols[i] = $('<div class="container" id="col'+ i +'"></div>')
   }
   $('#main').append($cols);
 }
+
+function createItem () {
+	return $('<div>', {
+		'class': 'item',
+		'style': 'background-color: ' + getItemColor() + '; height: ' + getItemHeight() + 'px'
+	})
+}
+
+function getItemHeight () {
+	return 50 + Math.floor(Math.random() * 300);
+}
+
+function getItemColor () {
+	var color = '#';
+	while(color.length < 7) {
+		var r = Math.floor(Math.random() * 256);
+		if ( r < 16 ) {
+			color += '0'
+		}
+		color += r.toString(16)
+	}
+	return color;
+}
+// 获取最小高度列
+function getMinHeightOfCol () {
+	
+	var $minCol = $cols[0];
+	var len = $cols.length
+	for (var i = 1; i < len; i++) {
+		if ($minCol.height() > $cols[i].height()) {
+			$minCol = $cols[i];
+		}
+	}
+	return $minCol;
+}
+// 创建Item，并把它加入最低的col内
+function appendItem () {
+	var $item = createItem();
+	var $minHeightCol = getMinHeightOfCol();
+	$minHeightCol.append($item);
+}
+
 main();
--- waterflow/app/server.js
+++ waterflow/app/server.js
@@ -1,8 +1,18 @@
+import $ from jquery;
+
 let server = function() {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      resolve();
-    }, 1000);
-  })
+  var dfd = $.Deferred();
+  // 模拟数据异步加载
+  setTimeout(function() {
+    def.resolve();
+  }, 1000);
+  
+  return dfd.promise();
+  // return new Promise(function(resolve, reject) {
+  //   setTimeout(function() {
+  //     resolve();
+  //   }, 1000);
+  // })
+
 };
 export default server;
